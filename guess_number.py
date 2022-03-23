b5c375818384e7d73fe739a4a35151a2e8c118a2
Ejsanchez <erik.sanchez.maldonado@gmail.com>
2022-03-23 16:31:00 -0600

    Create guess number program

---
+++ guess_number.py
@@ -0,0 +1,24 @@
+import random
+import math
+
+#Calcular el número
+number = math.ceil((random.random() * 1000)) + 1
+
+print("\nBienvenido, el sistema 'pensará' un número entre 1 y 1000 y deberás adivinarlo\n")
+
+#Solicitar el número
+guess = -1
+
+while guess != number :
+
+    guess = int(input("Introduce el número: "))
+
+    if guess < number :
+
+        print("Cerca! el número por adivinar es mayor.\n")
+
+    elif guess > number :
+
+        print("Cerca! el número por adivinar es menor.\n")
+
+print(f"\nFelicidades! adivinaste el número '{number}'!\n")
